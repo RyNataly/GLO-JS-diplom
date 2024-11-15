f7408d4593581036bf903eb0369b8e97cabaf6b1
Natalia Ryzhenko <ryzhena@gmail.com>
2024-11-15 20:38:39 +0300

    2-закрытие модального окна

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -2,12 +2,21 @@ export const modal = () => {
   const buttonCallbacks = document.querySelectorAll('.callback-btn')
   const modalCallback = document.querySelector('.modal-callback')
   const modalOverlay = document.querySelector('.modal-overlay')
+  const modalClose = document.querySelector('.modal-close')
 
   buttonCallbacks.forEach(buttonCallback => buttonCallback.addEventListener('click', (e) => {
       e.preventDefault();   
       modalCallback.style.display = 'flex'
       modalOverlay.style.display = 'flex'
-      console.log(modalOverlay)
   }))
-  
+
+  modalClose.addEventListener('click', () => {
+      modalCallback.style.display = 'none'
+      modalOverlay.style.display = 'none'   
+  })
+
+  modalOverlay.addEventListener('click', () => {
+      modalCallback.style.display = 'none'
+      modalOverlay.style.display = 'none'   
+  })
 }
