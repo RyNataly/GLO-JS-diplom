e237f3b91d73d9571b9924c38890992a900ef8a6
Natalia Ryzhenko <ryzhena@gmail.com>
2024-11-18 21:26:46 +0300

    7-Аккордион в блок вопросов

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ import { modal } from './src/modules/modal'
 import { menu } from './src/modules/menu'
 import { slider } from './src/modules/slider'
 import { services } from './src/modules/services'
+import { accordeon } from './src/modules/accordeon'
 
 modal()
 menu()
 slider()
 services()
+accordeon()
