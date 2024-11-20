2830d0506a65f4115777a7a5b18b5f514e86586c
Natalia Ryzhenko <ryzhena@gmail.com>
2024-11-20 22:40:25 +0300

    анимация модального окна

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,3 +1,5 @@
+import { animate } from "./helpers";
+
 export const modal = () => {
   const buttonCallbacks = document.querySelectorAll(".callback-btn");
   const modalCallback = document.querySelector(".modal-callback");
@@ -7,9 +9,32 @@ export const modal = () => {
   const elements = servicesCarousel.querySelectorAll(".element");
   const buttonServices = document.querySelector(".button-services");
 
+  const height = document.documentElement.clientHeight * 0.1;
+  let topBlock = height * 20;
+
+  const animating = () => {
+    animate({
+      duration: 400,
+      timing(timeFraction) {
+        return timeFraction;
+      },
+      draw(progress) {
+        modalCallback.style.top = topBlock * (1 - progress) + height + "px";
+      },
+    });
+  };
+
   const modalView = () => {
     modalCallback.style.display = "flex";
     modalOverlay.style.display = "flex";
+    const width = document.documentElement.clientWidth;
+    if (width > 768) {
+      topBlock = height * 10;
+      modalCallback.style.top = topBlock + "px";
+      animating();
+    } else {
+      modalCallback.style.top = height;
+    }
   };
 
   const modalHide = () => {
